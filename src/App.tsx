403c9903be651a8e3d827828118b42efc2c1d259
Michelle Tani <michelletani@outlook.com>
2025-09-25 15:18:41 -0300

    ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './clock/clock';
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
@@ -11,21 +12,14 @@ export class App extends React.Component {
   state = {
     hasClock: true,
     clockName: 'Clock-0',
-    today: new Date(),
   };
 
   timerId?: number;
 
-  timeUpdatedInterval?: number;
-
-  componentDidMount(): void {
+  componentDidMount() {
     this.timerId = window.setInterval(() => {
       this.setState({ clockName: getRandomName() });
     }, 3300);
-
-    this.timeUpdatedInterval = window.setInterval(() => {
-      this.setState({ today: new Date() });
-    }, 1000);
 
     document.addEventListener('contextmenu', this.handleRightClock);
     document.addEventListener('click', this.handleLeftClick);
@@ -40,40 +34,22 @@ export class App extends React.Component {
     this.setState({ hasClock: true });
   };
 
-  componentWillUnmount(): void {
+  componentWillUnmount() {
     if (this.timerId) {
       clearInterval(this.timerId);
     }
 
-    if (this.timeUpdatedInterval) {
-      clearInterval(this.timeUpdatedInterval);
-    }
-
     document.removeEventListener('contextmenu', this.handleRightClock);
     document.removeEventListener('click', this.handleLeftClick);
-
-    //console.log('Clock component unmounted');
   }
 
-  // This code starts a timer
+  render() {
+    const { hasClock, clockName } = this.state;
 
-  // this code stops the timer
-
-  render(): React.ReactNode {
     return (
       <div className="App">
         <h1>React clock</h1>
-        {this.state.hasClock && (
-          <div className="Clock">
-            <strong className="Clock__name">{this.state.clockName}</strong>
-
-            {' time is '}
-
-            <span className="Clock__time">
-              {this.state.today.toUTCString().slice(-12, -4)}
-            </span>
-          </div>
-        )}
+        {hasClock && <Clock name={clockName} />}
       </div>
     );
   }
